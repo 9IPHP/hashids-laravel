a7606494f642c4c13733ea796a6a2ed4183cdeae
shiyannan <shiyannan@octinn.com>
2016-06-29 16:35:05 +0800

    add config

---
+++ config/hashids.php
@@ -0,0 +1,9 @@
+<?php
+return [
+    // unique salt value
+    'hashids_salt' => '',
+    // minimum result length
+    'hashids_min_length' => 0,
+    // custom alphabet
+    'hashids_alphabet' => '',
+];
--- src/Specs/Hashids/HashidsServiceProvider.php
+++ src/Specs/Hashids/HashidsServiceProvider.php
@@ -5,7 +5,18 @@ class HashidsServiceProvider extends ServiceProvider {
     {
         $this->app->singleton('hashids', function()
         {
-            return new \Hashids\Hashids;
+            return new \Hashids\Hashids(config('hashids.hashids_salt', ''), config('hashids.hashids_min_length', 0), config('hashids.hashids_alphabet', ''));
         });
+    }
+
+    public function boot()
+    {
+        $configPath = __DIR__ . '/../config/hashids.php';
+        if (function_exists('config_path')) {
+            $publishPath = config_path('hashids.php');
+        } else {
+            $publishPath = base_path('config/hashids.php');
+        }
+        $this->publishes([$configPath => $publishPath], 'config');
     }
 }
